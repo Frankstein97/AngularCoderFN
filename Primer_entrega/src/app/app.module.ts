66314b6ee3def2cfbce76fd0027a8ecda6efe2ac
Frankstein97 <francoivannicoletti@gmail.com>
2023-09-14 18:11:56 -0300

    Componentes+Nicoletti

--- Primer_entrega/src/app/app.module.ts
+++ Primer_entrega/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MenuComponent } from './menu/menu.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    MenuComponent,
+    WrapperComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
